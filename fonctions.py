6e5d69c53c2cbbce8885b74458a08e0ea3c629ab
cofri <corentin.friedrich@polymtl.ca>
2014-09-10 17:31:07 -0400

    second commit

--- fonctions.py
+++ fonctions.py
@@ -4,23 +4,34 @@ from fonctions import *
 from main import *
 #sys.path.append("tkintertable-1.1/")
 #sys.path.append("pybtex-0.18/")
-sys.path.append("../tkintertable-1.1.2/")
+sys.path.append("../tkintertable-1.1/")
 sys.path.append("../pybtex/")
 from tkintertable.Tables import TableCanvas
 from tkintertable.TableModels import TableModel
 from pybtex.database.input import bibtex
 from copy import deepcopy
+import tkFileDialog
 
-def readBibFile(self,filename,frameRef,frameTag) :
+def readBibFile(filename,frameRef,frameTag) :
+	
+	global data,listTags
+	options = {}
+	options['filetypes'] =  [('bibtex files', '.bib'),('all files', '.*')]
+	filename = tkFileDialog.askopenfilename(**options)
+
 	print "Importing bibtex file ..."
-
 	parser = bibtex.Parser()
 	#bib_data = parser.parse_file('/users/cofri/IRCCyN_2014-07-17/Documents/These/Articles/biblio_local_old.bib')
-	bib_data = parser.parse_file('biblio_local_old.bib')
+	#bib_data = parser.parse_file('biblio_local_old.bib')
+	bib_data = parser.parse_file(filename)
 	data = {}
 	for i,entry in enumerate(bib_data.entries) :
 		uid = str(bib_data.entries.keys()[i])
 		tmp = bib_data.entries[bib_data.entries.keys()[i]]
+		try :
+			refType = str(tmp.type)
+		except :
+			refType = ''
 		try :
 			title = str(tmp.fields['title'])
 		except :
@@ -35,11 +46,15 @@ def readBibFile(self,filename,frameRef,frameTag) :
 			year = ''
 		try :
 			author = str(tmp.fields['author'])
+			author = author.split(" and ")
+			author = [s.strip() for s in author]
 		except :
 			author = ''
-		data[uid] = {'UID':uid,'Title':title,'Journal':journal,'Year':year,'Authors':author,'Tags':['coucou','lol']}
+
+		data[uid] = {'UID':uid,'refType':refType,'Title':title,'Journal':journal,'Year':year,'Authors':author,
+			'Tags':['coucou','lol','neige','pluie']}
 		if uid == 'abubakar2012application' :
-			data[uid]['Tags'] = ['arthur','lol']
+			data[uid]['Tags'] = ['arthur','lol','blabla','jacob']
 
 	"""
 	model = TableModel()
@@ -49,6 +64,8 @@ def readBibFile(self,filename,frameRef,frameTag) :
 	table.createTableFrame()
 	table.redrawTable()
 	"""
+	print "readBibFile"
+	print data.keys()
 	drawTable(data,frameRef)
 	list_tags(data)
 	display_tags(data,frameRef,frameTag)
@@ -58,65 +75,277 @@ def drawTable(data,frame) :
 
 	model = TableModel()
 	model.importDict(data)
-	model.columnNames = ['UID','Title','Authors','Journal','Year','Tags']
+	model.columnNames = ['UID','refType','Title','Authors','Journal','Year','Tags']
 	table = TableCanvas(frame, model=model)
 	table.createTableFrame()
 	table.redrawTable()
 
 
 def list_tags(data) :
-
+	global listTags
 	listTags = []
 	for key in data :
 	  for tag in data[key]['Tags'] :
 	    if not tag in listTags :
 	      listTags.append(tag)
-	print "list Tags = " 
-	print listTags
+	return listTags
+
+def append_tags(data) :
+	global listTags
+	for key in data :
+	  for tag in data[key]['Tags'] :
+	    if not tag in listTags :
+	      listTags.append(tag)
 	return listTags
 
 
 def display_tags(data,frameRef,frameTag) :
 
-	tags = list_tags(data)
-	print "type tag = " + str(type(tags))
-        dlist = [[tag,False] for tag in tags]
+	global listTags
+	#print "type tag = " + str(type(tags))
+        dlist = [[tag,False] for tag in listTags]
 
 	frameTag.grid()
-        for r in range(len(tags)):
+        for r in range(len(listTags)):
             frameTag.rowconfigure(r, weight=1)    
-        for c in range(2):
-            frameTag.columnconfigure(c, weight=1)
-        Label(frameTag, text="Tag", anchor="w",justify=LEFT,font=('bold',16)).grid(row=0, column=0, sticky="ew")
-        Label(frameTag, text="Filter",justify=RIGHT,font=('bold',16)).grid(row=0, column=1, sticky="ew")
+        Label(frameTag, text="TAGS",justify=CENTER,font=('bold',16)).grid(row=0, column=0, sticky="ew")
 
         row = 1
+	buttons = {}
         for (tag, active) in dlist:
-            nr_label = Label(frameTag, text=tag, anchor="w")
-            active_cb = Checkbutton(frameTag, onvalue=True, offvalue=False,command= lambda: filter_tag(data,frameRef,frameTag,tag,active_cb))
-            if active:
-                active_cb.select()
-            else:
-                active_cb.deselect()
-            nr_label.grid(row=row, column=0, sticky="ew")
-            active_cb.grid(row=row, column=1, sticky="ew")
+            #nr_label = Label(frameTag, text=tag, anchor="w")
+            #active_cb = Checkbutton(frameTag, onvalue=True, offvalue=False)#,command= lambda: filter_tag(data,frameRef,frameTag,tag,active_cb))
+            #if active:
+            #    active_cb.select()
+            #else:
+            #    active_cb.deselect()
+            #nr_label.grid(row=row, column=0, sticky="ew")
+            #active_cb.grid(row=row, column=1, sticky="ew")
+	    buttons[tag] = Button(frameTag,text=tag)
+	    buttons[tag].grid(row=row, column=0, sticky="ew")
+	    buttons[tag].configure(command=lambda tag=tag :filter_tag(data,listTags,frameRef,frameTag,buttons,tag))
+
             row += 1
 
+	#for key in buttons :
+	    #print buttons[key].cget('bg')
 
 
-def filter_tag(data,frameRef,frameTag,tag,active_cb) :
- print "Tag " + tag + " active? " + str(active_cb)
- tags_sel = [tag]
- refs_sel = {}
- print "Filtering refs with tags..."
- if active_cb == True :
-  for key in data :
-    for tag_sel in tags_sel :
-      if tag_sel in data[key]['Tags'] :
-        refs_sel[key] = data[key]
-        break
+def filter_tag(data,tags,frameRef,frameTag,buttons,tag) :
+	
+	print "Filtering tags..."
+	print tag
+	if buttons[tag].cget('bg') == "#d9d9d9" :
+	  buttons[tag].configure(bg = "red")
+	else :
+	  buttons[tag].configure(bg = "#d9d9d9")
+	#print buttons[tag].cget('bg')
 
- if len(refs_sel) == 0 :
-  refs_sel['rien'] = {'UID':'rien','Title':'rien','Journal':'rien','Year':'rien','Authors':'rien','Tags':['rien']}
- drawTable(refs_sel,frameRef)
+	tags_sel = []
+	for tag in buttons :
+	  if buttons[tag].cget('bg') == "red" :
+	    tags_sel.append(tag)
+	#print tags_sel
 
+	if tags_sel == [] :
+	  refs_sel = data
+	else :
+	  refs_sel = {}
+	  for key in data :
+	    nb_tag = 0
+	    for tag_sel in tags_sel :
+	      if tag_sel in data[key]['Tags'] :
+	        nb_tag += 1
+	    if nb_tag == len(tags_sel) :
+	      refs_sel[key] = data[key]
+	if len(refs_sel) == 0 :
+	  refs_sel = {'No refs':{'UID':'','refType':'','Title':'','Journal':'','Year':'','Authors':'',
+			'Tags':['']}}
+
+	drawTable(refs_sel,frameRef)
+
+
+def readRefFile(filename,frameRef,frameTag) :
+	
+    try :
+	global data,listTags
+	data = {}
+	print "readRefFile"
+	print data.keys()
+
+	options = {}
+	options['filetypes'] =  [('refM files', '.refm'),('all files', '.*')]
+	filename = tkFileDialog.askopenfilename(**options)
+
+	fs = open(filename,'rU')
+	text_fs = fs.read()
+	fs.close()
+	text_fs = text_fs.replace("\n","")
+	liste = text_fs.split("%ref")
+	del liste[0]
+
+	for s in liste :
+		s = "".join(s.split("@")[1:])
+		reftype = s.split("{")[0].lower()
+		uid = s.split(reftype.upper()+"{")[1].split(",")[0]
+		title = s.split("title = {")[1].split("},")[0]
+		journal = s.split("journal = {")[1].split("},")[0]
+		year = int(s.split("year = {")[1].split("},")[0])
+		authors = s.split("author = {")[1].split("},")[0]
+		authors = authors.split(" and ")
+		tags = s.split("tags = {")[1].split("}")[0]
+		if tags == '' :
+			tags = []
+		else :
+			tags = tags.split(" ; ")
+
+    		data[uid] = {'UID':uid,'refType':reftype,'Authors':authors,'Title':title,'Journal':journal,'Year':year,'Tags':tags}
+
+	print data.keys()
+	drawTable(data,frameRef)
+	list_tags(data)
+	display_tags(data,frameRef,frameTag)
+	return data
+    except :
+	""" Do nothing """
+
+
+def addRefFile(filename,frameRef,frameTag) :
+
+    try :	
+	global data,listTags
+	print "addRefFile"
+	print data.keys()
+
+	options = {}
+	options['filetypes'] =  [('refM files', '.refm'),('all files', '.*')]
+	filename = tkFileDialog.askopenfilename(**options)
+
+	fs = open(filename,'rU')
+	text_fs = fs.read()
+	fs.close()
+	text_fs = text_fs.replace("\n","")
+	liste = text_fs.split("%ref")
+	del liste[0]
+	for s in liste :
+		s = "".join(s.split("@")[1:])
+		reftype = s.split("{")[0].lower()
+		uid = s.split(reftype.upper()+"{")[1].split(",")[0]
+		title = s.split("title = {")[1].split("},")[0]
+		journal = s.split("journal = {")[1].split("},")[0]
+		year = int(s.split("year = {")[1].split("},")[0])
+		authors = s.split("author = {")[1].split("},")[0]
+		authors = authors.split(" and ")
+		tags = s.split("tags = {")[1].split("}")[0]
+		if tags == '' :
+			tags = []
+		else :
+			tags = tags.split(" ; ")
+
+    		data[uid] = {'UID':uid,'refType':reftype,'Authors':authors,'Title':title,'Journal':journal,'Year':year,'Tags':tags}
+
+	
+	print data.keys()
+	drawTable(data,frameRef)
+	append_tags(data)
+	display_tags(data,frameRef,frameTag)
+	return data
+
+    except :
+	""" Do nothing """
+
+
+###############################################################################################################
+def saveRefFile() :
+
+    global data
+
+    options = {}
+    options['filetypes'] =  [('refM files', '.refm')]
+    options['defaultextension'] = '.refm'
+    filename = tkFileDialog.asksaveasfilename(**options)
+
+    print "saving references to file " + filename + " ..."
+
+    fs = open(filename,'w')
+
+    for ref in data :
+	  fs.write("%ref\n")
+	  fs.write("@"+data[ref]['refType'].upper()+"{"+data[ref]['UID']+",\n")
+	  fs.write("  author = {")
+	  for ind,i in enumerate(data[ref]['Authors']) :
+	    if ind > 0 :
+	      fs.write(' and ')
+	    fs.write(i)
+	  fs.write("},\n")
+	  fs.write("  title = {" + data[ref]['Title'] + "},\n")
+	  fs.write("  journal = {" + data[ref]['Journal'] + "},\n")
+	  fs.write("  year = {" + str(data[ref]['Year']) + "},\n")
+	  fs.write("  tags = {")
+	  for ind,i in enumerate(data[ref]['Tags']) :
+	    if ind > 0 :
+	      fs.write(' ; ')
+	    fs.write(i)
+	  fs.write("}\n")
+	  fs.write("}\n")
+	  fs.write("\n")
+    fs.close()
+
+
+###############################################################################################################
+def createNewTag(frameRef,frameTag) :
+
+    global data, listTags
+
+    tagwin = Tk()
+    Label(tagwin,text="Give a name to a new tag :").pack()
+    newTag = Entry(tagwin,width = 25)
+    newTag.pack()
+    newTag.focus_set()
+    Button(tagwin,text="Enter",command=lambda : addNewTag(tagwin,newTag,frameRef,frameTag)).pack()
+
+    tagwin.mainloop()
+
+def addNewTag(tagwin,newTag,frameRef,frameTag) :
+	global data, listTags
+	print "new tag added"
+	print newTag.get()
+	listTags.append(newTag.get())
+	tagwin.destroy()
+	display_tags(data,frameRef,frameTag)
+
+
+###############################################################################################################
+def editRef(frameRef,frameTag) :
+
+    global data, listTags
+
+    editwin = Tk()
+    Label(editwin,text="Choose references :").pack()
+    listbox1 = Listbox(editwin,selectmode = "multiple")
+    listbox1.pack()
+    for ref in data :
+	listbox1.insert(END,ref)
+    selbut1 = Button(editwin,text="Select refs")
+    selbut1.pack()
+
+    Label(editwin,text="Choose tags :").pack(side=RIGHT)
+    listbox2 = Listbox(editwin,selectmode = "multiple")
+    listbox2.pack(side=RIGHT)
+    for tag in listTags :
+	listbox2.insert(END,tag)
+    selbut2 = Button(editwin,text="Select tags",command = lambda: addTagstoRefs(listbox2))
+    selbut2.pack(side=RIGHT)
+
+    editwin.mainloop()
+
+def addTagstoRefs(listbox) :
+    
+    global data, listTags
+    
+    items = listbox.curselection()
+    items = [listTags[int(item)] for item in items]
+    print items
+
+
+    
--- gui_final.py
+++ gui_final.py
@@ -4,12 +4,13 @@ from fonctions import *
 from main import *
 #sys.path.append("tkintertable-1.1/")
 #sys.path.append("pybtex-0.18/")
-sys.path.append("../tkintertable-1.1.2/")
+sys.path.append("../tkintertable-1.1/")
 sys.path.append("../pybtex/")
 from tkintertable.Tables import TableCanvas
 from tkintertable.TableModels import TableModel
 from pybtex.database.input import bibtex
 from copy import deepcopy
+import tkFileDialog
 
 ###############################################################################################
 ######## CLASS App ########
@@ -17,41 +18,41 @@ class App:
 
     def __init__(self, master):
 	
-
+	global data
+	global listTags
 	data = {}
+	listTags = []
 
 	# Define the grid
 	master.grid()
-	for r in range(2):
+	for r in range(5):
             master.rowconfigure(r, weight=1)    
         for c in range(4):
             master.columnconfigure(c, weight=1)
 	master.title('Coucou')
-	master.geometry(("1000x700"))
+	master.geometry(("1200x700"))
         
 	# Block for refs
 	frameRef = LabelFrame(master,padx=10,pady=10)
-	frameRef.grid(row=0,column=1,rowspan=2,columnspan=3,sticky="nwe")
+	frameRef.grid(row=0,column=1,rowspan=5,columnspan=3,sticky="nwes")
 
 	# Block for tags
 	frameTag = LabelFrame(master,padx=10,pady=10)
-        frameTag.grid(row=0,column=0)
+        frameTag.grid(row=0,column=0,rowspan=4)
 	#ChoixTag(frameTag)
-	display_tags(data,frameRef,frameTag)
+	#display_tags(data,frameRef,frameTag)
 
-	# Block test
+	# Block quit
 	frameQuit = Frame(master,bg="green")
-	frameQuit.grid(row=1,column=0,sticky="ewns")
-	Button(frameQuit,text="QUIT",command=master.quit,bg="red").pack(fill=BOTH,expand=1)
+	frameQuit.grid(row=5,column=0,sticky="ewns")
+	Button(frameQuit,text="QUIT",command=lambda:master.destroy(),bg="red").pack(fill=BOTH,expand=1)
 		
 	# Main Menu
 	menu = MainMenu(master,frameRef,frameTag)	
-	print "menu.data = "
-	print menu.data
 
 	# Block test 2
-	frame4 = Frame(master)
-	frame4.grid(row=1,column=1,columnspan=3,sticky="ewns")
+	#frame4 = Frame(master)
+	#frame4.grid(row=1,column=1,columnspan=3,sticky="ewns")
 	#table = TableCanvas(frame4)
 	#table.createTableFrame()
 	#data = {'rec1': {'col1': 99.88, 'col2': 108.79, 'label': 'rec1'},'rec2': {'col1': 99.88, 'col2': 108.79, 'label': 'rec2'}}
@@ -166,39 +167,36 @@ class VisuRef:
 class MainMenu :
 
   def __init__(self, master,frameRef,frameTag):
-	
-	global visu
-	self.data = {}
+
+	global data
 	self.frameRef = frameRef
 	menubar = Menu(master)
 	# create a pulldown menu, and add it to the menu bar
 	filemenu = Menu(menubar, tearoff=0)
-	filemenu.add_command(label="Open bibtex file", command= lambda : self.readBibFile('',frameRef,frameTag))
-	filemenu.add_command(label="Save", command=self.printText)
+	filemenu.add_command(label="Open bibtex file", command= lambda : readBibFile('',frameRef,frameTag))
+	filemenu.add_command(label="Open RefM file", command=lambda : readRefFile('',frameRef,frameTag))
+	filemenu.add_command(label="Add RefM file", command=lambda : addRefFile('',frameRef,frameTag))
+	filemenu.add_command(label="Save to RefM file", command=lambda : saveRefFile())
 	filemenu.add_command(label="Exit", command=master.quit)
 	menubar.add_cascade(label="File", menu=filemenu)
 
 	# create more pulldown menus
 	editmenu = Menu(menubar, tearoff=0)
-	editmenu.add_command(label="Cut", command=self.printText)
-	editmenu.add_command(label="Copy", command=self.printText)
-	editmenu.add_command(label="Paste", command=self.printText)
+	editmenu.add_command(label="Create new tag", command= lambda :createNewTag(frameRef,frameTag))
+	editmenu.add_command(label="Delete tag", command=lambda : printText())
+	editmenu.add_command(label="Edit reference", command=lambda : editRef(frameRef,frameTag))
 	menubar.add_cascade(label="Edit", menu=editmenu)
 
 	helpmenu = Menu(menubar, tearoff=0)
-	helpmenu.add_command(label="About", command=self.printText)
+	helpmenu.add_command(label="About", command=lambda : printText())
 	menubar.add_cascade(label="Help", menu=helpmenu)
 
 	# display the menu
 	master.config(menu=menubar)
 
-  def printText(self) :
+def printText() :
 	print "Coucou les clous"
-
-  def readBibFile(self,filename,frameRef,frameTag) :
-	self.data = readBibFile(self,filename,frameRef,frameTag)
-	print "self.data = "
-	print self.data
+	
 
 ###############################################################################################
 ######## main function ########
